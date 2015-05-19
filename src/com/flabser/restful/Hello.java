a6f98bc41570cf1879dea421b525f025bec71aee
flabser <info@flabser.com>
2015-05-19 10:51:37 +0300

    remove unused rules

--- src/com/flabser/restful/Hello.java
+++ src/com/flabser/restful/Hello.java
@@ -9,26 +9,23 @@ import javax.ws.rs.core.MediaType;
 @Path("/hello")
 public class Hello {
 
-  // This method is called if TEXT_PLAIN is request
   @GET
   @Produces(MediaType.TEXT_PLAIN)
   public String sayPlainTextHello() {
-    return "Hello Jersey";
+    return "Hello 2Next";
   }
 
-  // This method is called if XML is request
   @GET
   @Produces(MediaType.TEXT_XML)
   public String sayXMLHello() {
-    return "<?xml version=\"1.0\"?>" + "<hello> Hello Jersey" + "</hello>";
+    return "<?xml version=\"1.0\"?>" + "<hello> Hello 2Next" + "</hello>";
   }
 
-  // This method is called if HTML is request
   @GET
   @Produces(MediaType.TEXT_HTML)
   public String sayHtmlHello() {
-    return "<html> " + "<title>" + "Hello Jersey" + "</title>"
-        + "<body><h1>" + "Hello Jersey" + "</body></h1>" + "</html> ";
+    return "<html> " + "<title>" + "Hello 2Next" + "</title>"
+        + "<body><h1>" + "Hello 2Next" + "</body></h1>" + "</html> ";
   }
 
 } 
