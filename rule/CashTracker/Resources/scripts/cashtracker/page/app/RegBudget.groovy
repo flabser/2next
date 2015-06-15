cb67a3f53a3ab5c5f45439f08270bb8d3531df3e
medin84 <kalihanovm@gmail.com>
2015-06-15 12:12:08 +0600

    commit

--- rule/CashTracker/Resources/scripts/cashtracker/page/app/RegBudget.groovy
+++ rule/CashTracker/Resources/scripts/cashtracker/page/app/RegBudget.groovy
@@ -3,12 +3,12 @@ package cashtracker.page.app
 import com.flabser.script._Session
 import com.flabser.script._WebFormData
 import com.flabser.script.events._DoScript
-
 import com.flabser.script.*
 import com.flabser.users.*
 import com.flabser.solutions.cashtracker.*
 import com.flabser.solutions.cashtracker.constants.*
-import cashtracker.dao.impl.BudgetDAOImpl
+
+import cashtracker.dao.BudgetDAO;
 import cashtracker.model.Budget
 
 
@@ -17,7 +17,7 @@ class RegBudget extends _DoScript {
 	@Override
 	public void doProcess(_Session session, _WebFormData formData, String lang) {
 
-		def dao = new BudgetDAOImpl(session)
+		def dao = new BudgetDAO(session)
 		def budget = new Budget()
 
 		budget.setName(formData.getValueSilently("budgetname"))
@@ -28,8 +28,8 @@ class RegBudget extends _DoScript {
 		def res = dao.addBudget(budget)
 		if (res > 0) {
 			publishElement("process", "budget-registered")
-		}else {
-			publishElement("error","saving-failed")
+		} else {
+			publishElement("error"," saving-failed")
 		}
 	}
 }
