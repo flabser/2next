7519506a5321bfb749ff4cfa1a2ca121d00c19ea
medin84 <kalihanovm@gmail.com>
2015-06-19 13:41:33 +0600

    ch dde script

---
+++ src/com/flabser/solutions/cashtracker/constants/TransactionState.java
@@ -0,0 +1,28 @@
+package com.flabser.solutions.cashtracker.constants;
+
+public enum TransactionState {
+	UNKNOWN(0), CONFIRMED(1), PENDING(2);
+
+	private final int value;
+
+	private TransactionState(int value) {
+		this.value = value;
+	}
+
+	public static TransactionState stateOf(int value) {
+		switch (value) {
+		case 1:
+			return CONFIRMED;
+
+		case 2:
+			return PENDING;
+
+		default:
+			return UNKNOWN;
+		}
+	}
+
+	public int getCode() {
+		return value;
+	}
+}
