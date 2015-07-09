728d3db3ba888e5f48cd8ca036d8d5e9f8ec33af
medin84 <kalihanovm@gmail.com>
2015-07-09 17:45:12 +0600

    new user controller

--- webapps/Administrator/js/controllers/new_user.js
+++ webapps/Administrator/js/controllers/new_user.js
@@ -1,8 +1,15 @@
 AdminApp.NewUserController = Ember.ObjectController.extend({
     actions: {
         save: function(user) {
-            console.log("save new user" + user);
-            user.save();
+            console.log("save new user", user);
+
+            var newUser = this.store.createRecord('user', {
+                login: user.login,
+                pwd: user.pwd,
+                email: user.email
+            });
+            newUser.save();
+
             this.transitionTo('users');
         }
     }
