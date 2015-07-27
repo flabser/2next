be3ca8f2ee7742950b6ac3047c1a3f2ad564a107
flabser <info@flabser.com>
2015-07-27 13:56:46 +0300

    correct Role field

--- src/com/flabser/users/UserRole.java
+++ src/com/flabser/users/UserRole.java
@@ -1,81 +1,82 @@
 package com.flabser.users;
 
-import javax.xml.bind.annotation.XmlTransient;
+import java.io.Serializable;
 
 import com.flabser.rule.Role;
 
-import java.io.Serializable;
-
-public class UserRole implements Serializable{
+public class UserRole implements Serializable {
 	public static final long serialVersionUID = 1L;
-	
 
 	private String name;
 	private String description;
-	private String application;
+	private String appID;
 
-    public String getApplication() {
-        return application;
-    }
+	public String getApplication() {
+		return appID;
+	}
 
-    public void setApplication(String application) {
-        this.application = application;
-    }
+	public void setApplication(String application) {
+		this.appID = application;
+	}
 
-    public UserRole(Role role) {
+	public UserRole(Role role) {
 		name = role.name;
 		description = role.description;
-        application = role.appID;
+		appID = role.appID;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
-		
-	
+
 	public String getDescription() {
 		return description;
 	}
-	
+
 	public void setDescription(String description) {
-		this.description = description;		
+		this.description = description;
 	}
-	
-	
-	public int isRead(){		
+
+	public int isRead() {
 		return 1;
 	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof UserRole)) return false;
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (!(o instanceof UserRole)) {
+			return false;
+		}
 
-        UserRole userRole = (UserRole) o;
+		UserRole userRole = (UserRole) o;
 
-        if (application != null ? !application.equals(userRole.application) : userRole.application != null)
-            return false;
-        if (name != null ? !name.equals(userRole.name) : userRole.name != null) return false;
+		if (appID != null ? !appID.equals(userRole.appID) : userRole.appID != null) {
+			return false;
+		}
+		if (name != null ? !name.equals(userRole.name) : userRole.name != null) {
+			return false;
+		}
 
-        return true;
-    }
+		return true;
+	}
 
-    @Override
-    public int hashCode() {
-        int result = name != null ? name.hashCode() : 0;
-        result = 31 * result + (application != null ? application.hashCode() : 0);
-        return result;
-    }
+	@Override
+	public int hashCode() {
+		int result = name != null ? name.hashCode() : 0;
+		result = 31 * result + (appID != null ? appID.hashCode() : 0);
+		return result;
+	}
 
-    public String toString() {
+	@Override
+	public String toString() {
 
 		return name;
 	}
 
-	
-	
 }
