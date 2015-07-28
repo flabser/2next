52b369491de72308503a98b40d69913c3c95d48b
dserikbayev <daryn.serikbayev@gmail.com>
2015-07-28 21:00:12 +0600

    regex

--- src/com/flabser/filters/RequestURL.java
+++ src/com/flabser/filters/RequestURL.java
@@ -1,11 +1,19 @@
 package com.flabser.filters;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class RequestURL {
 	String appName;
 	String appID;
 
 	RequestURL(String url) {
-
+		Pattern pattern = Pattern.compile("^/(\\p{Alpha}+)/([\\p{Lower}0-9]+)/[\\p{Alpha}\\.]+$");
+		Matcher matcher = pattern.matcher(url);
+		if (matcher.matches()) {
+			appName = matcher.group(1);
+			appID = matcher.group(2);
+		}
 	}
 
 }
