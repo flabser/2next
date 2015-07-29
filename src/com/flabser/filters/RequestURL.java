ff2e39dc9b0639e21edcbc8469d85cc5737b233b
dserikbayev <daryn.serikbayev@gmail.com>
2015-07-29 15:49:29 +0600

    existence of appID not necessary

--- src/com/flabser/filters/RequestURL.java
+++ src/com/flabser/filters/RequestURL.java
@@ -8,11 +8,13 @@ public class RequestURL {
 	String appID;
 
 	RequestURL(String url) {
-		Pattern pattern = Pattern.compile("^/(\\p{Alpha}+)/([\\p{Lower}0-9]+)/[\\p{Alpha}\\.]+$");
+		Pattern pattern = Pattern.compile("^/(\\p{Alpha}+)/([\\p{Lower}0-9]{16}/)?[\\w\\.]+$");
 		Matcher matcher = pattern.matcher(url);
 		if (matcher.matches()) {
 			appName = matcher.group(1);
 			appID = matcher.group(2);
+			if(appID != null)
+				appID = appID.substring(0, appID.length() - 1);
 		}
 	}
 
