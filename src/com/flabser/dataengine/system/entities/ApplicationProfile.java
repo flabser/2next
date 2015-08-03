dea9dac1451d94598d3d238e8d10e819362c6967
flabser <info@flabser.com>
2015-08-03 14:25:35 +0300

    ш

--- src/com/flabser/dataengine/system/entities/ApplicationProfile.java
+++ src/com/flabser/dataengine/system/entities/ApplicationProfile.java
@@ -104,7 +104,7 @@ public class ApplicationProfile implements _IContent {
 		ISystemDatabase sysDatabase = DatabaseFactory.getSysDatabase();
 
 		if (id == 0) {
-			appID = "_" + Util.generateRandomAsText("qwertyuiopasdfghjklzxcvbnm1234567890", 14) + "_";
+			appID = Util.generateRandomAsText("qwertyuiopasdfghjklzxcvbnm1234567890");
 			id = sysDatabase.insert(this);
 		} else {
 			id = sysDatabase.update(this);
