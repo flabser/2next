03cc1bc36a7bc6e7596167a82b018c90ae6c9aab
medin84 <kalihanovm@gmail.com>
2015-08-07 13:55:58 +0600

    mod field

--- rule/CashTracker/Resources/scripts/cashtracker/init/FirstAction.java
+++ rule/CashTracker/Resources/scripts/cashtracker/init/FirstAction.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import com.flabser.dataengine.IAppDatabaseInit;
 import com.flabser.restful.Application;
 
+
 public class FirstAction implements IAppDatabaseInit {
 
 	@Override
@@ -16,8 +17,8 @@ public class FirstAction implements IAppDatabaseInit {
 	}
 
 	@Override
-	public ArrayList<String> getTablesDDE() {
-		ArrayList<String> result = new ArrayList<>();
+	public ArrayList <String> getTablesDDE() {
+		ArrayList <String> result = new ArrayList <>();
 
 		result.add(getBudgetDDE());
 		result.add(getAccountDDE());
@@ -30,7 +31,7 @@ public class FirstAction implements IAppDatabaseInit {
 	}
 
 	@Override
-	public ArrayList<String> getInitActions() {
+	public ArrayList <String> getInitActions() {
 		// INFO заполнение базовых значений (справочников, ...)
 		return null;
 	}
@@ -40,11 +41,11 @@ public class FirstAction implements IAppDatabaseInit {
 		sql.append("CREATE TABLE BUDGETS ");
 		sql.append("(");
 		sql.append("  ID         SERIAL NOT NULL,\n");
-		sql.append("  NAME       CHARACTER VARYING(64),\n");
+		sql.append("  NAME       CHARACTER VARYING(128),\n");
 		sql.append("  REGDATE    TIMESTAMP WITHOUT TIME ZONE DEFAULT now(),\n");
 		sql.append("  OWNER      CHARACTER VARYING(128),\n");
 		sql.append("  STATUS     NUMERIC,\n");
-		sql.append("  CONSTRAINT BUDGET_ID_PK PRIMARY KEY (ID)");
+		sql.append(" CONSTRAINT BUDGET_ID_PK PRIMARY KEY (ID)");
 		sql.append(")");
 		return sql.toString();
 	}
@@ -54,17 +55,18 @@ public class FirstAction implements IAppDatabaseInit {
 		sql.append("CREATE TABLE ACCOUNTS ");
 		sql.append("(");
 		sql.append("  ID                  SERIAL NOT NULL,\n");
-		sql.append("  NAME                TEXT,\n");
+		sql.append("  NAME                CHARACTER VARYING(128),\n");
 		sql.append("  TYPE                SMALLINT,\n");
 		sql.append("  CURRENCY_CODE       CHARACTER VARYING(3),\n");
 		sql.append("  OPENING_BALANCE     NUMERIC,\n");
 		sql.append("  AMOUNT_CONTROL      NUMERIC,\n");
-		sql.append("  OWNER CHARACTER     VARYING(128),\n");
+		sql.append("  OWNER               CHARACTER VARYING(128),\n");
+		sql.append("  ENABLED             BOOLEAN,\n");
 		sql.append("  OBSERVERS           TEXT[],\n");
 		sql.append("  INCLUDE_IN_TOTALS   BOOLEAN,\n");
-		sql.append("  NOTE                TEXT,\n");
+		sql.append("  NOTE                CHARACTER VARYING(256),\n");
 		sql.append("  SORT_ORDER          SMALLINT,\n");
-		sql.append("  CONSTRAINT ACCOUNT_ID_PK PRIMARY KEY (ID)");
+		sql.append(" CONSTRAINT ACCOUNT_ID_PK PRIMARY KEY (ID)");
 		sql.append(")");
 		return sql.toString();
 	}
@@ -76,14 +78,15 @@ public class FirstAction implements IAppDatabaseInit {
 		sql.append("  ID                 SERIAL NOT NULL,\n");
 		sql.append("  TRANSACTION_TYPE   SMALLINT,\n");
 		sql.append("  PARENT_ID          BIGINT,\n");
-		sql.append("  NAME               CHARACTER VARYING(256),\n");
-		sql.append("  NOTE               TEXT,\n");
+		sql.append("  ENABLED            BOOLEAN,\n");
+		sql.append("  NAME               CHARACTER VARYING(128),\n");
+		sql.append("  NOTE               CHARACTER VARYING(256),\n");
 		sql.append("  COLOR              SMALLINT,\n");
 		sql.append("  SORT_ORDER         SMALLINT,\n");
-		sql.append("  CONSTRAINT CATEGORY_ID_PK PRIMARY KEY (ID), ");
-		sql.append("  CONSTRAINT CATEGORY_PARENT_ID_FK FOREIGN KEY (PARENT_ID)\n");
-		sql.append("    REFERENCES CATEGORIES (ID) MATCH SIMPLE\n");
-		sql.append("    ON UPDATE NO ACTION ON DELETE NO ACTION");
+		sql.append(" CONSTRAINT CATEGORY_ID_PK PRIMARY KEY (ID), ");
+		sql.append(" CONSTRAINT CATEGORY_PARENT_ID_FK FOREIGN KEY (PARENT_ID)\n");
+		sql.append("   REFERENCES CATEGORIES (ID) MATCH SIMPLE\n");
+		sql.append("   ON UPDATE NO ACTION ON DELETE NO ACTION");
 		sql.append(")");
 		return sql.toString();
 	}
@@ -93,8 +96,8 @@ public class FirstAction implements IAppDatabaseInit {
 		sql.append("CREATE TABLE COSTCENTERS ");
 		sql.append("(");
 		sql.append("  ID         SERIAL NOT NULL,\n");
-		sql.append("  NAME       CHARACTER VARYING(256),\n");
-		sql.append("  CONSTRAINT COSTCENTER_ID_PK PRIMARY KEY (ID)");
+		sql.append("  NAME       CHARACTER VARYING(128),\n");
+		sql.append(" CONSTRAINT COSTCENTER_ID_PK PRIMARY KEY (ID)");
 		sql.append(")");
 		return sql.toString();
 	}
@@ -104,8 +107,8 @@ public class FirstAction implements IAppDatabaseInit {
 		sql.append("CREATE TABLE TAGS ");
 		sql.append("(");
 		sql.append("  ID         SERIAL NOT NULL,\n");
-		sql.append("  NAME       CHARACTER VARYING(64),\n");
-		sql.append("  CONSTRAINT TAG_ID_PK PRIMARY KEY (ID)");
+		sql.append("  NAME       CHARACTER VARYING(32),\n");
+		sql.append(" CONSTRAINT TAG_ID_PK PRIMARY KEY (ID)");
 		sql.append(")");
 		return sql.toString();
 	}
@@ -125,30 +128,29 @@ public class FirstAction implements IAppDatabaseInit {
 		sql.append("  EXCHANGE_RATE       NUMERIC,\n");
 		sql.append("  CATEGORY            BIGINT,\n");
 		sql.append("  COST_CENTER         BIGINT,\n");
-		sql.append("  TAGS                CHARACTER VARYING(64)[],\n");
+		sql.append("  TAGS                CHARACTER VARYING(32)[],\n");
 		sql.append("  REPEAT              BOOLEAN,\n");
 		sql.append("  EVERY               NUMERIC,\n");
 		sql.append("  REPEAT_STEP         SMALLINT,\n");
 		sql.append("  START_DATE          TIME WITHOUT TIME ZONE,\n");
 		sql.append("  END_DATE            TIME WITHOUT TIME ZONE,\n");
-		sql.append("  BASIS               BYTEA,\n");
-		sql.append("  NOTE                TEXT,\n");
+		sql.append("  NOTE                CHARACTER VARYING(256),\n");
 		sql.append("  INCLUDE_IN_REPORTS  BOOLEAN,\n");
-		sql.append("  CONSTRAINT TRANSACTION_ID_PK PRIMARY KEY (ID),\n");
-		sql.append("  CONSTRAINT TRANSACTION_ACCOUNT_FROM_FK FOREIGN KEY (ACCOUNT_FROM)\n");
-		sql.append("    REFERENCES ACCOUNTS (ID) MATCH SIMPLE\n");
-		sql.append("    ON UPDATE NO ACTION ON DELETE NO ACTION,\n");
-		sql.append("  CONSTRAINT TRANSACTION_ACCOUNT_TO_FK FOREIGN KEY (ACCOUNT_TO)\n");
-		sql.append("    REFERENCES ACCOUNTS (ID) MATCH SIMPLE\n");
-		sql.append("    ON UPDATE NO ACTION ON DELETE NO ACTION,\n");
-		sql.append("  CONSTRAINT TRANSACTION_CATEGORY_FK FOREIGN KEY (CATEGORY)\n");
-		sql.append("    REFERENCES CATEGORIES (ID) MATCH SIMPLE\n");
-		sql.append("    ON UPDATE NO ACTION ON DELETE NO ACTION,\n");
-		sql.append("  CONSTRAINT TRANSACTION_COST_CENTER_FK FOREIGN KEY (COST_CENTER)\n");
-		sql.append("    REFERENCES COSTCENTERS (ID) MATCH SIMPLE\n");
-		sql.append("    ON UPDATE NO ACTION ON DELETE NO ACTION");
+		sql.append("  BASIS               BYTEA,\n");
+		sql.append(" CONSTRAINT TRANSACTION_ID_PK PRIMARY KEY (ID),\n");
+		sql.append(" CONSTRAINT TRANSACTION_ACCOUNT_FROM_FK FOREIGN KEY (ACCOUNT_FROM)\n");
+		sql.append("   REFERENCES ACCOUNTS (ID) MATCH SIMPLE\n");
+		sql.append("   ON UPDATE NO ACTION ON DELETE NO ACTION,\n");
+		sql.append(" CONSTRAINT TRANSACTION_ACCOUNT_TO_FK FOREIGN KEY (ACCOUNT_TO)\n");
+		sql.append("   REFERENCES ACCOUNTS (ID) MATCH SIMPLE\n");
+		sql.append("   ON UPDATE NO ACTION ON DELETE NO ACTION,\n");
+		sql.append(" CONSTRAINT TRANSACTION_CATEGORY_FK FOREIGN KEY (CATEGORY)\n");
+		sql.append("   REFERENCES CATEGORIES (ID) MATCH SIMPLE\n");
+		sql.append("   ON UPDATE NO ACTION ON DELETE NO ACTION,\n");
+		sql.append(" CONSTRAINT TRANSACTION_COST_CENTER_FK FOREIGN KEY (COST_CENTER)\n");
+		sql.append("   REFERENCES COSTCENTERS (ID) MATCH SIMPLE\n");
+		sql.append("   ON UPDATE NO ACTION ON DELETE NO ACTION");
 		sql.append(")");
 		return sql.toString();
 	}
-
 }
