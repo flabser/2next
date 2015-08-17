075e0a5dc7833221a6115454ed9d96cf703b3184
flabser <info@flabser.com>
2015-08-17 15:53:33 +0300

    it was fixed app registration functionally and changed database version from 9.2 to 9.4

--- src/com/flabser/users/ApplicationStatusType.java
+++ src/com/flabser/users/ApplicationStatusType.java
@@ -1,22 +1,21 @@
 package com.flabser.users;
 
 public enum ApplicationStatusType {
-	UNKNOWN(0), ON_LINE(895), READY_TO_REMOVE(896), DEPLOING_FAILED(897), DATABASE_NOT_CREATED(898);
-
+	UNKNOWN(0), ON_LINE(895), READY_TO_REMOVE(896), DEPLOING_FAILED(897), DATABASE_NOT_CREATED(898), READY_TO_DEPLOY(899);
 
 	private int code;
 
-	ApplicationStatusType(int code){
+	ApplicationStatusType(int code) {
 		this.code = code;
 	}
 
-	public int getCode(){
+	public int getCode() {
 		return code;
 	}
 
-	public static ApplicationStatusType getType(int code){
-		for (ApplicationStatusType type : values()){
-			if (type.code == code){
+	public static ApplicationStatusType getType(int code) {
+		for (ApplicationStatusType type : values()) {
+			if (type.code == code) {
 				return type;
 			}
 		}
