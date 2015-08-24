2e53cfcf3488ffddc37c6bc279f4811b8b5d8957
flabser <info@flabser.com>
2015-08-24 23:41:57 +0300

    commented unused tests

--- rule/CashTracker/Resources/scripts/cashtracker/test/init/FirstActionTest.java
+++ rule/CashTracker/Resources/scripts/cashtracker/test/init/FirstActionTest.java
@@ -1,21 +1,14 @@
 package cashtracker.test.init;
 
-import java.io.IOException;
-
-import org.apache.http.client.ClientProtocolException;
 import org.junit.Assert;
-import org.junit.Test;
-
-import cashtracker.init.FirstAction;
-
 
 public class FirstActionTest extends Assert {
 
-	@Test
-	public void test() throws ClientProtocolException, IOException {
-
-		FirstAction fa = new FirstAction();
-
-		fa.getTablesDDE().forEach(System.out::println);
-	}
+	/*
+	 * @Test public void test() throws ClientProtocolException, IOException {
+	 * 
+	 * FirstAction fa = new FirstAction();
+	 * 
+	 * fa.getTablesDDE().forEach(System.out::println); }
+	 */
 }
--- rule/CashTracker/Resources/scripts/cashtracker/test/rest/TagServiceTest.java
+++ rule/CashTracker/Resources/scripts/cashtracker/test/rest/TagServiceTest.java
@@ -1,36 +1,22 @@
 package cashtracker.test.rest;
 
-import java.io.IOException;
-
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-
 
 public class TagServiceTest extends Assert {
 
-	String url = "http://localhost:38779/CashTracker/-/rest/session";
-	CloseableHttpClient client;
-
-	@Before
-	public void init() {
-		client = HttpClientBuilder.create().build();
-	}
-
-	@After
-	public void end() throws IOException {
-		client.close();
-	}
-
-	@Test
-	public void test() throws ClientProtocolException, IOException {
-
-		HttpPost post = new HttpPost(url);
-
-	}
+	/*
+	 * String url = "http://localhost:38779/CashTracker/-/rest/session";
+	 * CloseableHttpClient client;
+	 * 
+	 * @Before public void init() { client = HttpClientBuilder.create().build();
+	 * }
+	 * 
+	 * @After public void end() throws IOException { client.close(); }
+	 * 
+	 * @Test public void test() throws ClientProtocolException, IOException {
+	 * 
+	 * HttpPost post = new HttpPost(url);
+	 * 
+	 * }
+	 */
 }
