e21f30039bde3a73e5ec3174c53d284717bd1a47
medin84 <kalihanovm@gmail.com>
2015-09-11 23:53:39 +0600

    em src

--- webapps/CashTracker/app/components/user-profile.js
+++ webapps/CashTracker/app/components/user-profile.js
@@ -4,28 +4,36 @@ import ModelForm from '../mixins/model-form';
 import Validate from '../utils/validator';
 
 export default Em.Component.extend(ModelForm, {
-    userProfile: null,
+    user: null,
+    changePassword: false,
+    needPassword: false,
+    isEdit: false,
 
     actions: {
         save: function() {
             if (this.validate()) {
-                this.sendAction('save', this.get('userProfile'));
+                this.sendAction('save', this.get('user'));
             }
         },
-
         cancel: function() {
             this.sendAction('cancel');
         },
-
         addAttach: function(attach) {
-            this.get('userProfile').attachedFile = attach;
+            this.get('user').avatar = attach;
+        },
+        toggleChangePassword: function() {
+            this.set('changePassword', !this.get('changePassword'));
+            this.set('needPassword', this.get('changePassword'));
+        },
+        setEdit: function() {
+            this.set('isEdit', true);
         }
     },
 
     validate: function() {
         this.set('errors', DS.Errors.create());
 
-        if (Validate.isEmpty(this.get('userProfile.name'))) {
+        if (Validate.isEmpty(this.get('user.name'))) {
             this.get('errors').add('name', this.get('i18n').t('validation_empty'));
         }
 
