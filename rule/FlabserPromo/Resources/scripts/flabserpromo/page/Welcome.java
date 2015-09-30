dea48b09dcfefe36630dec71115d4fd0846c157d
medin84 <kalihanovm@gmail.com>
2015-09-30 17:27:54 +0600

    flabser promo

---
+++ rule/FlabserPromo/Resources/scripts/flabserpromo/page/Welcome.java
@@ -0,0 +1,40 @@
+package flabserpromo.page;
+
+import com.flabser.localization.LanguageType;
+import com.flabser.script._AppEntourage;
+import com.flabser.script._Exception;
+import com.flabser.script._Session;
+import com.flabser.script._WebFormData;
+import com.flabser.script.events._DoScript;
+
+
+public class Welcome extends _DoScript {
+
+	@Override
+	public void doGet(_Session session, _WebFormData formData, String lang) throws _Exception {
+
+		String toLang = formData.getValueSilently("lang");
+		try {
+			if (toLang.length() > 0) {
+				session.switchLang(LanguageType.valueOf(toLang.toUpperCase()));
+			}
+		} catch (Exception e) {
+			publishElement("error", "the " + toLang + " language is not available");
+		}
+
+		_AppEntourage ent = session.getAppEntourage();
+		publishElement("availablelangs", ent.getAvailableLangs());
+	}
+
+	@Override
+	public void doPost(_Session session, _WebFormData formData, String lang) {
+	}
+
+	@Override
+	public void doPut(_Session session, _WebFormData formData, String lang) {
+	}
+
+	@Override
+	public void doDelete(_Session session, _WebFormData formData, String lang) {
+	}
+}
