92e607ab8cbc4aa57ecde836607feeba7b7d6493
medin84 <kalihanovm@gmail.com>
2015-10-09 23:17:37 +0600

    +lang

--- src/com/flabser/localization/LanguageType.java
+++ src/com/flabser/localization/LanguageType.java
@@ -1,23 +1,25 @@
 package com.flabser.localization;
 
-
+/**
+ * https://ru.wikipedia.org/wiki/%D0%9A%D0%BE%D0%B4%D1%8B_%D1%8F%D0%B7%D1%8B%D0%BA%D0%BE%D0%B2
+ * numeric code by ГОСТ 7.75-97
+ */
 public enum LanguageType {
-	UNKNOWN(0), ENG(325), RUS(326), KAZ(327), BG(328);
-	
-	
+	UNKNOWN(0), ENG(45), RUS(570), KAZ(255), BG(115), POR(545), SPA(230), CHO(315);
+
 	private int code;
-	
-	LanguageType(int code){
+
+	LanguageType(int code) {
 		this.code = code;
-	}	
-	
-	public int getCode(){
+	}
+
+	public int getCode() {
 		return code;
 	}
-	
-	public static LanguageType getType(int code){
-		for (LanguageType type : values()){
-			if (type.code == code){
+
+	public static LanguageType getType(int code) {
+		for (LanguageType type : values()) {
+			if (type.code == code) {
 				return type;
 			}
 		}
