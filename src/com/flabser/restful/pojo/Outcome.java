6002674435821dc4b7c39f00308933c742fea5de
flabser <info@flabser.com>
2015-10-29 09:56:10 +0200

    fix  message sending

--- src/com/flabser/restful/pojo/Outcome.java
+++ src/com/flabser/restful/pojo/Outcome.java
@@ -79,4 +79,5 @@ public class Outcome {
 		}
 		return this;
 	}
+
 }
--- src/com/flabser/util/RestUtil.java
+++ src/com/flabser/util/RestUtil.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.core.Response;
 
+import com.flabser.exception.ServerServiceExceptionType;
 import com.flabser.restful.pojo.Outcome;
 import com.flabser.script._Session;
 import com.flabser.script._Validator;
@@ -19,7 +20,7 @@ public class RestUtil {
 		ArrayList<String> e = validateSimpleMailForm(email, subj, msg, captcha);
 
 		if (e.size() > 0) {
-
+			res.setMessage(ServerServiceExceptionType.FORMDATA_INCORRECT, lang);
 			return Response.status(HttpServletResponse.SC_BAD_REQUEST).entity(res.setMessages(e, lang)).build();
 		}
 
@@ -27,9 +28,15 @@ public class RestUtil {
 		ArrayList<String> recipients = new ArrayList<String>();
 		recipients.add(recipient);
 		_MailAgent ma = session.getMailAgent();
-		ma.sendMail(recipients, subj, msg + " (lang: " + lang + " from: " + email + ")", false);
+		if (ma.sendMail(recipients, subj, msg + " (lang: " + lang + " from: " + email + ")", false)) {
+			res.addMessage("message_has_sent_succesfully", lang);
+			return Response.status(HttpServletResponse.SC_OK).entity(res).build();
+		} else {
+			res.setMessage(ServerServiceExceptionType.MESSAGE_HAS_NOT_SENT, lang);
+			return Response.status(HttpServletResponse.SC_INTERNAL_SERVER_ERROR).entity(res.setMessages(e, lang))
+					.build();
+		}
 
-		return Response.status(HttpServletResponse.SC_OK).entity(res).build();
 	}
 
 	private static ArrayList<String> validateSimpleMailForm(String email, String subj, String msg, String captcha) {
@@ -38,7 +45,7 @@ public class RestUtil {
 
 		if (!_Validator.checkEmail(email)) {
 			isValid = false;
-			errors.add("email_incorrect");
+			errors.add(ServerServiceExceptionType.EMAIL_IS_INCORRECT.name());
 		}
 		if (subj.isEmpty() || subj.length() > 256) {
 			isValid = false;
