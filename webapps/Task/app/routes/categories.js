fe9b4eb25458633a23624ddbe8293a6850cdd7c6
medin84 <kalihanovm@gmail.com>
2015-10-31 20:25:40 +0600

    cat

---
+++ webapps/Task/app/routes/categories.js
@@ -0,0 +1,30 @@
+import Em from 'ember';
+
+export default Em.Route.extend({
+    model: function() {
+        return this.store.findAll('category');
+    },
+
+    actions: {
+        composeRecord: function() {
+            this.transitionTo('categories.new');
+        },
+
+        saveRecord: function(category) {
+            category.save().then(() => {
+                this.transitionTo('categories');
+            }, function() {
+                console.log(arguments);
+            });
+        },
+
+        deleteRecord: function(category) {
+            category.destroyRecord().then(() => {
+                this.transitionTo('categories');
+            }, function(resp) {
+                category.rollbackAttributes();
+                alert(resp.errors.message);
+            });
+        }
+    }
+});
