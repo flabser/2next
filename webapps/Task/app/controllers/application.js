986a6255bc274b811a74e94d533e5aa23b065c5e
medin84 <kalihanovm@gmail.com>
2015-11-13 23:40:27 +0600

    t

--- webapps/Task/app/controllers/application.js
+++ webapps/Task/app/controllers/application.js
@@ -2,4 +2,13 @@ import Em from 'ember';
 
 export default Em.Controller.extend({
 
+    filterTags: Em.computed.filter('tags', function(tag, index, array) {
+        /*let list = this.get('tags');
+
+        return list.filter((t) => {
+            let t_isNew = t.get('isNew');
+            return !t_isNew;
+        });*/
+        return !tag.get('isNew');
+    })
 });
