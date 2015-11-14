0ac4daf04fc71883fc53737c93b075d6da2875fc
medin84 <kalihanovm@gmail.com>
2015-11-14 18:07:46 +0600

    t

--- webapps/Task/app/controllers/issues.js
+++ webapps/Task/app/controllers/issues.js
@@ -1,6 +1,16 @@
 import Em from 'ember';
 
 export default Em.Controller.extend({
+    i18n: Em.inject.service(),
+
     queryParams: ['at', 'st', 'tags', 'u'],
-    at: 'all'
+    at: 'all',
+
+    headerTitle: Em.computed('at', function() {
+        let at = this.get('at');
+        if (at === 'all') {
+            return this.get('i18n').t('issues_all');
+        }
+        return this.get('i18n').t(at);
+    })
 });
